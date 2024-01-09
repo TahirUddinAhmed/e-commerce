5a6fce2014735e1afcf3e38ccbd8677e049c4ac3
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-01-09 11:01:19 +0530

    Index file code change

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <?php require_once 'includes/header.php' ?>
-      <!-- Banner section -->
-      <?php require_once 'includes/_banner-section.php' ?>
-      <!-- !Banner section -->
+<?php
+// <!-- Banner section -->
+require_once 'includes/_banner-section.php';
+// <!-- !Banner section -->
 
-      <!-- start #top section -->
-      <?php require_once 'includes/_top-sale.php' ?>
-      <!-- !start #top section -->
+// <!-- start #top section -->
+require_once 'includes/_top-sale.php';
+// <!-- !start #top section -->
 
-      <!-- Special-price -->
-      <?php require_once 'includes/_spacial-offer.php' ?>
-      <!-- !Special-price -->
+// <!-- Special-price -->
+require_once 'includes/_spacial-offer.php'; 
+// <!-- !Special-price -->
 
-      <!-- Banner ads -->
-      <?php require_once 'includes/_banner-ads.php' ?>
-      <!-- !Banner ads -->
+// <!-- Banner ads -->
+require_once 'includes/_banner-ads.php';
+// <!-- !Banner ads -->
 
-      <!-- New product -->
-      <?php require_once 'includes/_new-product.php' ?>
-      <!-- !New product -->
+// <!-- New product -->
+require_once 'includes/_new-product.php';
+// <!-- !New product -->
 
-      <!-- Blog Section -->
-      <?php require_once 'includes/_blog-section.php'?>
-      <!-- !Blog Section -->
+// <!-- Blog Section -->
+require_once 'includes/_blog-section.php';
+// <!-- !Blog Section -->
+?>
 
 <?php require_once 'includes/footer.php' ?>
