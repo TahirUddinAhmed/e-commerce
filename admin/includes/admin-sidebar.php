452522c34dcadcee059c0b73391a358c86ff715d
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-01-26 11:50:55 +0530

    set up admin dashboard

---
+++ admin/includes/admin-sidebar.php
@@ -0,0 +1,46 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#product"><i class="fa-solid fa-box"></i> Product Management <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="product" class="collapse">
+                            <li>
+                                <a href="#">Add product</a>
+                            </li>
+                            <li>
+                                <a href="#">Categories</a>
+                            </li>
+                            <li>
+                                <a href="#">View products</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="#"><i class="fa-solid fa-truck-fast"></i> Orders </a>
+                    </li>
+
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#customer"><i class="fa-solid fa-users"></i> Customer Management <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="customer" class="collapse">
+                            <li>
+                                <a href="#">Add Customers</a>
+                            </li>
+                            <li>
+                                <a href="#">View Customers</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa-solid fa-chart-simple"></i> Analytics and Reporting </a>
+                    </li>  
+                    <hr>
+                    <li>
+                        <a href="#"><i class="fa-solid fa-gear"></i> Admin </a>
+                    </li>
+                    
+                </ul>
+            </div>
---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<?php include('includes/admin-header.php') ?>
+
+<!-- Page Heading -->
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">
+            Welcome to E-commerce Dashboard
+            <!-- <small></small> -->
+        </h1>
+        <!-- <ol class="breadcrumb">
+            <li>
+                <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
+            </li>
+            <li class="active">
+                <i class="fa fa-file"></i> Blank Page
+            </li>
+        </ol> -->
+    </div>
+</div>
+<!-- /.row -->
+
+</div>
+<!-- /.container-fluid -->
+<?php include('includes/admin-footer.php'); ?>
