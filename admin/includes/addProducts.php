84029e30ea463a003d7f939d1961f26af37a2ff9
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-01-26 21:33:20 +0530

    add product form validate

--- admin/includes/addProducts.php
+++ admin/includes/addProducts.php
@@ -1,10 +1,40 @@
+<?php
+//  add product 
+ if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // grab all the input fields
+    $productName = mysqli_real_escape_string($conn, $_POST['product-name']);
+    $sellingPrice = mysqli_real_escape_string($conn, $_POST['product-price']);
+    $originalPrice = mysqli_real_escape_string($conn, $_POST['original-price']);
+    $productCategory = mysqli_real_escape_string($conn, $_POST['product-category']);
+    $productDesc = mysqli_real_escape_string($conn, $_POST['product-desc']);
+    $productStatus = mysqli_real_escape_string($conn, $_POST['product-status']);
+    
+    // validate the form 
+    if(empty($productName) || empty($sellingPrice) || empty($originalPrice) || empty($productCategory) || empty($productDesc) || empty($productStatus)) {
+        $formErr = "All fields are required";
+    }
+    
+ }
+
+
+ // query to retrieve all categories 
+ $query = "SELECT * FROM category";
+ $category_result = mysqli_query($conn, $query);
+
+ if(!$category_result) {
+    die("QUERY FAILED" . mysqli_error($conn));
+ }
+
+
+?>
 <h2>Add Products</h2>
 <hr>
 
 <div class="row">
-    <form action="" method="post">
+    <form action="products.php?source=add" method="post">
         <div class="col-lg-7" style="border: 1.5px solid #ddd; margin-left: 20px; padding: 14px;">
-         <div class="form-group">
+        <span class="text-danger"><?= $formErr ?? null ?></span> 
+        <div class="form-group">
              <label for="product-name">Product Name</label>
              <input type="text" name="product-name" class="form-control">
          </div>
@@ -19,11 +49,21 @@
          <div class="form-group">
             <label for="product-category">Product Category</label>
              <select name="product-category" class="form-control">
-                <option value="">Choose Category</option>
-                <option value="">Choose Category</option>
-                <option value="">Choose Category</option>
-                <option value="">Choose Category</option>
-                <option value="">Choose Category</option>
+                 <option value="">Choose Category</option>
+
+             <?php
+                if(mysqli_num_rows($category_result) > 0) {
+                    while($data = mysqli_fetch_assoc($category_result)) {
+                        $cat_id = $data['id'];
+                        $cat_name = $data['name'];
+            ?>
+                    <option value="<?= $cat_id ?>"><?= $cat_name ?></option>
+
+            <?php
+                    }
+                }
+             ?>
+                
              </select>
          </div>
 
@@ -43,8 +83,8 @@
         <div class="col-lg-4" style="border: 1.5px solid #ddd; margin-left: 20px; padding: 14px;">
          <div class="">
             <div class="form-group">
-                <label for="Visibility">Product Status</label>
-                <select name="visibility" class="form-control">
+                <label for="product-status">Product Status</label>
+                <select name="product-status" class="form-control">
                     <option value="">Select</option>
                     <option value="draft">draft</option>
                     <option value="public">Public</option>
