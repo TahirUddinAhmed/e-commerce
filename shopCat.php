2ecccb38de013b9b9a3792dd93c0c48d41e36bb7
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-02-11 06:59:58 +0530

    shop page completed

--- shopCat.php
+++ shopCat.php
@@ -15,6 +15,14 @@
 ?>
 <section id="special-price">
         <div class="container shop mt-5">
+          <div class="back-btn">
+            <a href="shop.php" class="nav-link border rounded-pill">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-left" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708z"/>
+            </svg>
+            Back
+            </a>
+          </div>
           <h4 class="font-rubik font-20">
             <?php
               if(isset($categoryId)) {
@@ -25,6 +33,8 @@
                       echo $catList['name'];
                    }
                 }
+              } else {
+                header("Location: shop.php");
               }
             ?>
           </h4>
@@ -52,6 +62,7 @@
           </div>
 
           <div class="grid">
+          
             <?php
               if(isset($categoryId)) {
                 
@@ -93,7 +104,7 @@
             
               }else {
                 ?>
-                    <p>Sorry No item</p>
+                    <p class="text-center mt-5 font-rale font-20">There are currently no items/products available for this category. </p>
                 <?php
                 }
             } 
